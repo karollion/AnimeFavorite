b5ec9822565073739d52249346a54347f87c9033
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2026-01-30 21:30:38 +0100

    add anime controller and rename models

---
+++ controllers/animes.controller.js
@@ -0,0 +1,31 @@
+const Anime = require('../models/anime.model');
+
+// Load all animes
+exports.getAll = async (req, res) => {
+  try {
+    const animes = await Anime.find()
+      .populate({ path: "characters", select: "name role image_url" })
+      .populate({ path: "seasons", select: "season_number title year" })
+      .sort({ createdAt: -1 })
+
+    res.json(animes)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+};
+
+exports.getOne = async (req, res) => {
+  try {
+    const anime = await Anime.findById(req.params.id)
+      .populate("characters")
+      .populate("seasons")
+
+    if (!anime) {
+      return res.status(404).json({ message: "Anime not found" })
+    }
+
+    res.json(anime)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
+};
