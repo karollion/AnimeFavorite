69956f6cc0c91682f2e3fea68645d1efcd521e8f
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2026-01-31 21:53:52 +0100

    add auth routes

---
+++ routes/auth.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const authMiddleware = require('../utils/authMiddleware');
+const auth = require('../controllers/auth.controller');
+
+/** 
+router.post('/register', imageUpload.single('avatar') , auth.register);
+
+router.post('/login', auth.login);
+
+router.get('/user', authMiddleware, auth.getUser);
+
+router.delete('/logout', authMiddleware, auth.logout);
+*/
+
+module.exports = router;
+
