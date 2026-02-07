2dd78b1b2c937e13b67291f2dbf8544e9b922970
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2026-02-07 22:07:29 +0100

    Add to client navbar and react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
+import { Routes, Route, useLocation } from 'react-router-dom';
+import NavBar from './components/views/NavBar/NavBar';
 import Footer from './components/views/Footer/Footer';
 
 function App() {
   return (
     <div>
+      <NavBar />
       <header>
         <h1>
           Favorite Anime
---
+++ client/src/components/views/NavBar/NavBar.js
@@ -0,0 +1,11 @@
+import styles from './NavBar.module.scss'
+
+const NavBar = () => {
+  return (
+    <div className={styles.root}>
+      <h2>navbar</h2>
+    </div>
+  );
+};
+
+export default NavBar;
