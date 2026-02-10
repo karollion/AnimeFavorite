ab731b004bcb459c43a0b4a745be2e6f3b4ea8f6
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2026-02-10 21:21:21 +0100

    Add NewAnimes module and no-image asset

--- client/src/components/features/AnimeCard/AnimeCard.js
+++ client/src/components/features/AnimeCard/AnimeCard.js
@@ -1,28 +1,22 @@
 import styles from './AnimeCard.module.scss';
 import { Col } from 'react-bootstrap';
 import PropTypes from 'prop-types'
+import noImage from '../../../assets/no-image.png'
 
 const AnimeCard = ({ anime }) => {
-  /*
-  const navigate = useNavigate();
-
-  const handleClick = e => {
-    e.preventDefault();
-    navigate("/anime/" + anime.slug);
-  }
-
-  <div className={styles.buttonBox}>
-    <Button variant="primary" action={handleClick}>Read more</Button>
-  </div>
-  */
 
   return (
     <Col xs='12' sm='6' md='4' lg='3' className='mb-4'>
       <div className={styles.card}>
-        <div className={styles.title}><p>{anime.age_rating}$</p></div>
-        <div className={styles.title}><p>{anime.rating_overall}$</p></div>
-        <img variant='top' alt='anime_image' src={anime.anime_cover} className={styles.img} />
-			  <div className={styles.body}>
+        <div ><p>Age rating: {anime.age_rating}</p></div>
+        <div ><p>Rating: {anime.rating_overall}</p></div>
+			  <img
+          variant='top'
+          alt={anime.title}
+          src={anime.anime_cover || noImage}
+          className={styles.img}
+        />
+        <div className={styles.body}>
           <p>{anime.title}</p>
           <p> {anime.type}</p>
         </div>
--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,9 +1,13 @@
 import styles from './Home.module.scss';
+import NewAnimes from '../../features/NewAnimes/NewAnimes';
+
 
 const Home = () => { 
+
   return (
     <div className={styles.root}>
       <h2>Home</h2>
+        <NewAnimes />
     </div>
   );
 };
