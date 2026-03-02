b69f9cebf7efbf5f57d62b34fcfe4e65ccd974f5
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2026-03-02 21:28:35 +0100

    Changes in backend

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,15 +3,15 @@ const router = express.Router();
 const authMiddleware = require('../utils/authMiddleware');
 const auth = require('../controllers/auth.controller');
 
-/** 
-router.post('/register', imageUpload.single('avatar') , auth.register);
+
+router.post('/register' , auth.register);
 
 router.post('/login', auth.login);
 
 router.get('/user', authMiddleware, auth.getUser);
 
 router.delete('/logout', authMiddleware, auth.logout);
-*/
+
 
 module.exports = router;
 
