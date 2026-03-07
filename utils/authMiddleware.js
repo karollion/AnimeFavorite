9cb556092ec03ec92cf74fb448fc87d292e90d50
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2026-03-07 21:21:26 +0100

    hange authMiddleware

--- utils/authMiddleware.js
+++ utils/authMiddleware.js
@@ -1,6 +1,12 @@
 module.exports = (req, res, next) => {
+
   if (!req.session.user) {
-    return res.status(401).json({ message: "Not authenticated" });
+    return res.status(401).json({ message: "Not authenticated" })
   }
-  next();
-};
+
+  if (req.session.user.role !== "admin") {
+    return res.status(403).json({ message: "Admin access required" })
+  }
+
+  next()
+}
