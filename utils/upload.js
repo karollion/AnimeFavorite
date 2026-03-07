e4946a039dfa1ea63875237a369b1666fd4c6f57
Karol Bernatowicz <karol.bernatowicz@gmail.com>
2026-03-07 22:11:28 +0100

    fix in upload.js

--- utils/upload.js
+++ utils/upload.js
@@ -1,6 +1,6 @@
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
-const cloudinary = require("../config/cloudinary");
+const cloudinary = require("./cloudinary");
 
 // funkcja generująca storage dla konkretnego folderu
 const createStorage = (folder) => {
